03a129a4217fcdc311d8513c2cb90fca9f4c058b
809961436 <809961436@qq.com>
2018-08-23 14:43:56 +0800

    带参数的轮播插件

--- js/JQuery.ZYSlide.js
+++ js/JQuery.ZYSlide.js
@@ -5,7 +5,7 @@
     //也就是说只会产生一个轮播图，这个函数的作用域只能分配给一个轮播图
     //所以要求在调用本函数的时候请务必将当前轮播图和标签传递过来
 
-    var slide = function (ele) {
+    var slide = function (ele, options) {
         //转为jQ标签对象
         var $ele = $(ele)
         //默认的设置选项
@@ -15,6 +15,12 @@
             //控制time的时间（轮播速度）
             speed: 2000
         };
+        //对象合并
+        //para1:是否深度合并，若为true且多个对象性的某个同名属性也是对象，则该“属性对象”的属性也将进行合并
+        //para2,para3,........都是要合并的对象
+    
+        $.extend(true, setting, options)
+
         var states = [
             { ZIndex: 1, width: 120, height: 150, top: 69, left: 134, opac: 0.2 },
             { ZIndex: 2, width: 130, height: 170, top: 59, left: 0, opac: 0.5 },
@@ -55,6 +61,7 @@
                 next();
             }, setting.speed)
         }
+        autoPlay()
         //停止轮播
         $ele.find('section').add(lis).hover(function () {
             clearInterval(time)
@@ -62,13 +69,11 @@
             autoPlay()
         })
     };
-    $.fn.ZYSlide = function () {
-        $(this).each(function(i,ele){
-            slide(ele)
+    $.fn.ZYSlide = function (options) {
+        $(this).each(function (i, ele) {
+            slide(ele, options)
         })
     }
-
-
 })(jQuery)
 //用jquery封装插件的几种写法：
 /*
